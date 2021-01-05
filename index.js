b677948239b8b869bff54f81f916a3917ed7c55c
Paul Cortes <paulkerg@gmail.com>
2021-01-05 16:13:36 -0500

    conexion a BD mongo remota

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require('express');
 const app = express();
 
 const { config } = require('./config/index');
+const moviesApi = require('./routes/movies');
+
+app.use(express.json());
+
+moviesApi(app);
 
 app.get('/', function(req, res) {
   res.send('Hello world');
