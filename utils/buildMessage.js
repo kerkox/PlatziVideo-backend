8fce6aab26cea99b380acb6cbf3f0bfc06a8abb0
Paul Cortes <paulkerg@gmail.com>
2021-01-07 20:34:06 -0500

    test de util

--- utils/buildMessage.js
+++ utils/buildMessage.js
@@ -1,5 +1,8 @@
 function buildMessage(entity, action) { // eslint-disable-line
-
+  if(action === 'list') {
+    return `${entity}s ${action}ed`;
+  }
+  return `${entity} ${action}d`;
 }
 
 module.exports = buildMessage;
